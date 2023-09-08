1c6f2d467a9d5f645f7cec30d15cda1d9855cff6
caleb <shawcaleb1688@gmail.com>
2023-09-08 14:17:18 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+const apiURL = 'https://appac.github.io/mlb-data-api-docs/' //url to link to api. not link to fetch
+
 function App() {
   return (
     <div className="App">
