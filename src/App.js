49b5f8aee190da8f7a2841b1ee7f844db9b50690
caleb <shawcaleb1688@gmail.com>
2023-09-08 21:50:50 -0400

    added thunk and title

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 const apiURL = 'https://official-joke-api.appspot.com/random_joke' 
 
+//i want to get a joke and show the setup  and when you click on the joke the punchline folds down
+
 function App() {
   return (
     <div className="App">
