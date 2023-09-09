20afbb1b9121cd164616a92351ad53f315df6ba8
caleb <shawcaleb1688@gmail.com>
2023-09-09 06:36:08 -0400

    added provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { legacy_createStore as createStore } from 'redux';
 import thunk from 'redux-thunk'
 
-const apiURL = 'https://official-joke-api.appspot.com/random_joke' 
+//const apiURL = 'https://official-joke-api.appspot.com/random_joke' 
 
 //i want to get a joke and show the setup  and when you click on the joke the punchline folds down
 
 function App() {
+
+
+  
   return (
     <div className="App">
       Want To Hear a Joke?
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 
+const store = createStore(reducer);
+
 ReactDOM.render(
+  <Provider store={store}>
   <App />,
-  document.getElementById('root')
+  {document.getElementById('root')}
+  </Provider>
 );
